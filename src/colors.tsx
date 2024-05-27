1df7705b3c2f2ef7956631a51fee61eee1a0ffb6
Keshav Mohta <xkeshav@gmail.com>
2024-05-27 17:49:47 +0530

    path param example

--- src/colors.tsx
+++ src/colors.tsx
@@ -1,12 +1,30 @@
 import { Hono } from 'hono';
+import { css, Style } from 'hono/css';
 
-const color = new Hono();
+const colors = new Hono();
 
-// MARK: HTML support
-color.get('/rainbow', (c) => {
+// MARK: HTML & CSS support
+colors.get('/', (c) => {
+  return c.text('type path `rainbow`')
+});
+
+colors.get('/rainbow', (c) => {
+  const headerClass = css`
+    background-color: orange;
+    color: white;
+    padding: 1rem;
+  `
   return c.html(
-    <div>
-      <h1>Color in Rainbow</h1>
+      <>
+      <Style>{
+      css`
+          ul > li {
+            font-size: 2rem;
+          }
+        `}
+      </Style>
+      <div>
+      <h1 class={headerClass}>Color in Rainbow</h1>
       <ul>
         <li>Violet</li>
         <li>Indigo</li>
@@ -15,9 +33,36 @@ color.get('/rainbow', (c) => {
         <li>Yellow</li>
         <li>Orange</li>
         <li>Red</li>
-      </ul> 
-    </div>
+      </ul>
+    </div></>
   );
 });
 
-export default color;
+// path param read
+colors.get('/:what?', (c) => {
+  const what  = c.req.param('what');
+  console.log({what});
+  return c.html(
+    <html>
+    <head>
+      <Style>{
+      css`
+      div {
+        background-color: ${what};
+        color: color-mix(in lch, white, pink);
+        mix-blend-mode: difference;
+        text-align: center;
+        font-size: 2rem;
+        padding: 1rem;
+        height: auto;
+        border: 1px solid grey;
+      }
+      `}
+      </Style>
+    </head>
+      <body> <div>custom path param value is : {what}</div> </body>
+    </html>
+    )
+});
+
+export default colors;
--- src/index.tsx
+++ src/index.tsx
@@ -1,49 +1,19 @@
 import { serve } from '@hono/node-server'; // for Node.js Adapter
 import { Hono } from 'hono';
 import { showRoutes } from 'hono/dev';
-//import colors from './colors';
 
+import colors from './colors';
 //import { Element } from './Element';
 
-////import { Alphabet } from './Alphabet';
-////import { alphabet } from '@xkeshav/alphabet';
+//import { alphabet } from '@xkeshav/alphabet';
+//import { prettyJSON } from 'hono/pretty-json';
+//import { Alphabet } from './Alphabet';
 
 const app = new Hono();
 
 // text
 app.get('/', (c) => c.text('Hello Node.js!'));
-app.on('GET', ['/hello', '/hi', '/hey'], (c) => c.text('🙋🏻‍♂️ Hello' ))
-
-// MARK: json support
-app.get('/element', (c) => {
-  return c.json([
-    {
-      id: 1,
-      name: 'Water',
-      emoji: "💧"
-    },
-    {
-      id: 2,
-      name: 'Fire',
-      emoji: "🔥"
-    },
-    {
-      id: 3,
-      name: 'Earth',
-      emoji: "🌍"
-    },
-    {
-      id: 4,
-      name: 'Air',
-      emoji: "💨"
-    },
-    {
-      id: 5,
-      name: 'Sky',
-      emoji: '🌌'
-    },
-  ]);
-});
+app.on('GET', ['/hello', '/hi', '/hey'], (c) => c.text('🙋🏻‍♂️ Hello' ));
 
 const element_list  = [
   {
@@ -72,16 +42,22 @@ const element_list  = [
     emoji: '🌌'
   },
 ];
+// MARK: json support
+//app.get('/element', prettyJSON(), (c) => {
+//  return c.json(element_list);
+//});
 
+// MARK: HTML support
 //app.get('/element/html', (c) => {
 //  return c.html(<Element list={element_list} />)
 //});
 
+// MARK: JSX Support
 //app.get('/alphabet', (c) => {
 //  return c.html(<Alphabet list={alphabet}/>)
 //});
 
-//app.route('/color', colors); // app.route
+app.route('/color', colors); // app.route
 
 showRoutes(app);
 serve(app);
